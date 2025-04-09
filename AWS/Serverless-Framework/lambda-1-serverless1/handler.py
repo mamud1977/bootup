04a577ce3a430161d0d159cd1de199061dd24e3c
Mamud Hasan <mail2mamud@gmail.com>
2025-04-09 16:14:15 -0500

    Serverless Framework

---
+++ AWS/Serverless-Framework/lambda-1-serverless1/handler.py
@@ -0,0 +1,20 @@
+import json
+import time
+import boto3
+
+
+
+
+def hello(event, context):
+    print("Hello World")
+    time.sleep(1)
+
+    return "response"
+
+
+def list_lambdas(event, context):
+    client = boto3.client('lambda')
+    response = client.list_functions()
+
+    print(response)
+    return response
