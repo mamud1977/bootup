66ea1f0c4c27fc7783f60e99f11bf69795069bbf
Mamud Hasan <mail2mamud@gmail.com>
2025-04-19 18:28:26 -0500

    Javascript

---
+++ 1-Javascript/1-variables.js
@@ -0,0 +1,16 @@
+// Variable declaration : var
+var greetings = 'Hello!';
+console.log('Hi...')
+
+// Variable declaration : let
+let varRate = 0.2;
+varRate = 0.5;
+console.log(varRate)
+
+// Constant declaration
+const fixedRate = 0.3;
+console.log(fixedRate)
+
+
+
+
