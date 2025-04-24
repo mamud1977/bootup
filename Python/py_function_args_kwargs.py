f50200a75e9d3b37484aeb22d1af4c569216f23d
Mamud Hasan <mail2mamud@gmail.com>
2025-04-24 00:38:00 -0500

    Python

---
+++ Python/py_function_args_kwargs.py
@@ -0,0 +1,6 @@
+def example_function(*argsmmmmm, **kwargsyyyy):
+    print("Positional arguments (args):", argsmmmmm)
+    print("Keyword arguments (kwargs):", kwargsyyyy)
+
+example_function(1, 2, 3, name="Abrar", age=13)
+example_function(10, 20, 30, 40, name="Mamud", age=47, height=169)
