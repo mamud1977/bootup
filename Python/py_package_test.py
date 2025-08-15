05787c5a3080751c3120393489b8c82bb888f600
Mamud Hasan <mail2mamud@gmail.com>
2025-08-15 16:33:34 +0530

    handson

--- Python/py_package_test.py
+++ Python/py_package_test.py
@@ -1,7 +1,11 @@
 from py_package import mainpkg1
 from py_package import mainpkg2
 from py_package.subpkg2 import subpkg2
+from py_package.subpkg3.subpkg3 import fn_subpkg3
     
 mainpkg1.fn_mainpkg()
 mainpkg2.fn_mainpkg()
 subpkg2.fn_subpkg2()
+
+
+ret = fn_subpkg3('{"name": "Abrar"}')
---
+++ Python/pytest_pkg_test.py
@@ -0,0 +1,7 @@
+import pytest
+
+from py_package.subpkg3.subpkg3 import fn_subpkg3
+
+def test_fn_subpkg3():
+   assert fn_subpkg3('{"name": "Mamud", "age": 40}') == True
+
