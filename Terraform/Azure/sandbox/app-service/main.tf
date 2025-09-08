38ca0cc52fea56b93686bf376b0b04f029590ca8
Mamud Hasan <mail2mamud@gmail.com>
2025-09-08 23:11:40 +0530

    AZ Web App

---
+++ Terraform/Azure/sandbox/app-service/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.50.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
+
+resource "azurerm_service_plan" "app_service_plan" {
+  name                = "webapp-free-plan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Windows"  # F1 only supports Windows
+  sku_name            = "B1"
+}
+
