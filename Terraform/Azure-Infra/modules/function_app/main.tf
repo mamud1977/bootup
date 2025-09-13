bafc656d1374e9df6a8cdaa9fbe0b28fcd45eaf2
Mamud Hasan <mail2mamud@gmail.com>
2025-09-13 22:27:08 +0530

    changes

--- Terraform/Azure-Infra/modules/function_app/main.tf
+++ Terraform/Azure-Infra/modules/function_app/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_function_app" "func" {
   name                       = var.function_app_name
   location                   = var.resource_group_location
   resource_group_name        = var.resource_group_name
-  service_plan_id            = azurerm_app_service_plan.plan.id
+  service_plan_id            = azurerm_service_plan.plan.id
   storage_account_name       = var.storage_account_name
   storage_account_access_key = data.azurerm_storage_account.primary_key.primary_access_key
   functions_extension_version = "~4"
