e42a4c6d4af6c55260b9b26240257b842d583a4e
Mamud Hasan <mail2mamud@gmail.com>
2025-09-13 12:12:31 +0530

    changes

--- Terraform/Azure-Infra/modules/storage/terraform.tfvars
+++ Terraform/Azure-Infra/modules/storage/terraform.tfvars
@@ -1,7 +1,7 @@
 # terraform.tfvars
 
-storage_account_name_1      = "mystorage5tgrffg"
+storage_account_name      = "mystorage5tgrffg"
 
-storage_container_name_1    = "mycontainerkd94dmkf9f"
+storage_container_name    = "mycontainerkd94dmkf9f"
 
 
--- Terraform/Azure-Infra/modules/storage/variables.tf
+++ Terraform/Azure-Infra/modules/storage/variables.tf
@@ -14,12 +14,12 @@ variable "resource_group_location" {
 }
 
 
-variable "storage_account_name_1" {
+variable "storage_account_name" {
   description = "Azure Storage Account Name"
   type        = string
 }
 
-variable "storage_container_name_1" {
+variable "storage_container_name" {
   description = "Name of the blob container"
   type        = string
 }
