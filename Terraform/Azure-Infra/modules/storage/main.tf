16f1cffeaa69a14ac16dae3440af40521e754355
Mamud Hasan <mail2mamud@gmail.com>
2025-09-13 21:12:33 +0530

    changes

--- Terraform/Azure-Infra/modules/storage/main.tf
+++ Terraform/Azure-Infra/modules/storage/main.tf
@@ -28,44 +28,9 @@ resource "azurerm_storage_account" "storage_account" {
   tags = var.tags
 }
 
-resource "azurerm_storage_management_policy" "cool_and_delete_snapshots" {
-  storage_account_id = azurerm_storage_account.storage_account.id
-
-  rule {
-    name    = "cool-after-45-days-delete-snapshots"
-    enabled = true
-
-    filters {
-      blob_types   = ["blockBlob"]
-      prefix_match = [""] # Empty string = all blobs
-    }
-
-    actions {
-      base_blob {
-        tier_to_cool {
-          days_since_last_access_time_greater_than = 45
-        }
-
-        delete {
-          days_after_creation_greater_than = 365 # Optional: delete base blob after 1 year
-        }
-      }
-
-      snapshot {
-        delete {
-          days_after_creation_greater_than = 0 # Delete all snapshots immediately
-        }
-      }
-    }
-  }
-}
-
-
 resource "azurerm_storage_container" "my_container" {
   name                    = var.container_name 
   storage_account_name    = azurerm_storage_account.storage_account.name               
   container_access_type   = "private"
-
-  tags = var.tags
 }
 
