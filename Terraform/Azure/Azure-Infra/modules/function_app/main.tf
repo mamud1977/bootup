cea0c26f903419a0ea410408c9f4cb5e375eb7e9
Mamud Hasan <mail2mamud@gmail.com>
2025-09-14 13:18:42 +0530

    changes

--- Terraform/Azure/Azure-Infra/modules/function_app/main.tf
+++ Terraform/Azure/Azure-Infra/modules/function_app/main.tf
@@ -22,7 +22,6 @@ resource "azurerm_linux_function_app" "func" {
   location                   = var.resource_group_location
   resource_group_name        = var.resource_group_name
   service_plan_id            = azurerm_service_plan.plan.id
-  storage_account_name       = var.storage_account_name
   
   storage_account_name       = var.storage_account_name
   storage_account_access_key = var.storage_account_access_key
