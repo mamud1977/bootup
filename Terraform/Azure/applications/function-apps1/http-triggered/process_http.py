73548aaf381e0d522baf41709c8e3cffc8d5556e
Mamud Hasan <mail2mamud@gmail.com>
2025-09-14 22:53:32 +0530

    changes

--- Terraform/Azure/applications/function-apps1/http-triggered/process_http.py
+++ Terraform/Azure/applications/function-apps1/http-triggered/process_http.py
@@ -5,29 +5,21 @@ from rapidfuzz import fuzz, process
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     
-    input_po_num = req.params.get("po_number")
-
-    choices = ["PO 12345", "POx 83n333", "PO 383838302"]
-
-    #Find top 3 matches
-    matches = process.extract(input_po_num, choices, limit=3)
-    print("Top matches:")
-    for match in matches:
-        print(match)
-
-
-    if name:
-        logging.info(f"Query parameter 'po_number' is present: {po_number}")
-    else:
-        logging.info("Query parameter 'po_number' is not present.")
-
     try:
-        req_body = req.get_json()
-        logging.info(f"Request body received: {req_body}")
-    except ValueError:
-        logging.info("No valid JSON body received.")
+        input_po_num = req.params.get("po_number")
 
-    return func.HttpResponse("Request processed.", status_code=200)
+        choices = ["PO 12345", "POx 83n333", "PO 383838302"]
+
+        #Find top 3 matches
+        matches = process.extract(input_po_num, choices, limit=3)
+        print("Top matches:")
+        for match in matches:
+            print(match)
+
+        return func.HttpResponse("Request processed.", status_code=200)
+    
+    except Exception as e:
+        return func.HttpResponse(f"Exception: {e}", status_code=500) 
 
 
 
