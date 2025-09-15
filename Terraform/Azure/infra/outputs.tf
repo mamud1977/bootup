4992a7053cfb01316e42360db9f51c977e522e20
Mamud Hasan <mail2mamud@gmail.com>
2025-09-15 22:06:10 +0530

    changes

---
+++ Terraform/Azure/infra/outputs.tf
@@ -0,0 +1,19 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.resource_group.name
+}
+
+output "resource_group_location" {
+  value = azurerm_resource_group.resource_group.location
+}
+
+output "env" {
+  value = var.env
+}
+
+output "tags" {
+  value = var.tags
+}
+
+output "random_string" {
+  value = random_string.storage_suffix.result
+}
