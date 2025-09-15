accefc15d9fab61f689e729ec3f40e8a53a7ef9d
Mamud Hasan <mail2mamud@gmail.com>
2025-09-15 22:19:36 +0530

    changes

--- Terraform/Azure/infra/main.tf
+++ Terraform/Azure/infra/main.tf
@@ -27,6 +27,7 @@ resource "random_string" "storage_suffix" {
 }
 
 
+
 locals {
   resource_group_name     = "${var.resource_group_name_1}-${var.env}"
   resource_group_location = "${var.resource_group_location}"
