cd12fdea7489d144eb8ebd69e5059208f4941104
Mamud Hasan <mail2mamud@gmail.com>
2025-09-16 12:37:05 +0530

    changes

--- Terraform/Azure/infra/modules/apim/main.tf
+++ Terraform/Azure/infra/modules/apim/main.tf
@@ -31,6 +31,6 @@ resource "azurerm_api_management_api" "function_api" {
 
   import {
     content_format = "swagger-link-json"
-    content_value  = ""https://${azurerm_linux_function_app.function_app.default_hostname}/api/swagger.json""
+    content_value  = "https://${azurerm_linux_function_app.function_app.default_hostname}/api/swagger.json"
   }
 }
