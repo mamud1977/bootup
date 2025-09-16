aada229912c3fc1ccc7a075a7df4d77369ae650e
Mamud Hasan <mail2mamud@gmail.com>
2025-09-16 18:33:08 +0530

    changes

--- Terraform/Azure/infra/modules/apim/main.tf
+++ Terraform/Azure/infra/modules/apim/main.tf
@@ -1,6 +1,6 @@
 # modules/apim/main.tf
 
-####### Create an APIM
+########### 1. Create an APIM
 
 resource "azurerm_api_management" "apim" {
   name                = var.apim_name
@@ -20,55 +20,17 @@ resource "azurerm_api_management" "apim" {
 }
 
 
-####### Create an API
+########### 2. Creating the APIM API resource
 
 resource "azurerm_api_management_api" "function_api" {
-  name                = "func-api-${var.env}"
+  name                = "hhtp-triggered-function-api-${var.env}"
   resource_group_name = var.resource_group_name
   api_management_name = azurerm_api_management.apim.name
   revision            = "1"
-  display_name        = "HTTP Function API"
-  path                = "function"
+  display_name        = "HTTP Function API - Best PO Number mathing"
+  path                = "best-po-number-matching"
   protocols           = ["https"]
 }
 
-####### Define operation
-
-resource "azurerm_api_management_api_operation" "match_po" {
-  operation_id        = "match-po"
-  api_name            = azurerm_api_management_api.function_api.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = var.resource_group_name
-  display_name        = "Match PO Number"
-  method              = "GET"
-  url_template        = "/match"
-  response {
-    status_code = 200
-    description = "Successful match"
-  }
-}
-
-###### Wire the Backend
-
-resource "azurerm_api_management_api_operation_policy" "match_po_policy" {
-  operation_id        = azurerm_api_management_api_operation.match_po.operation_id
-  api_name            = azurerm_api_management_api.function_api.name
-  api_management_name = azurerm_api_management.apim.name
-  resource_group_name = var.resource_group_name
-
-  xml_content = <<XML
-  <policies>
-  <inbound>
-    <base />
-    <set-backend-service base-url="https://${var.function_app_hostname}/api/match-po" />
-  </inbound>
-  <backend>
-    <base />
-  </backend>
-  <outbound>
-    <base />
-  </outbound>
-  </policies>
-  XML
-}
+########### Define operation
 
