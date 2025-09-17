524fd274ece2a9b0cf406f7c79771823d96470f2
Mamud Hasan <mail2mamud@gmail.com>
2025-09-17 09:51:54 +0530

    ch

--- Terraform/Azure/infra/main.tf
+++ Terraform/Azure/infra/main.tf
@@ -18,9 +18,9 @@ terraform {
 
 provider "azurerm" {
   features {
-  │     resource_group {
-│       prevent_deletion_if_contains_resources = false
-│     }
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
   }
 }
 
