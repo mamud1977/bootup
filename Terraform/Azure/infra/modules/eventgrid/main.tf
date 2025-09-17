9c0adb8400f7c70a2ccb6099546698557f710e8e
Mamud Hasan <mail2mamud@gmail.com>
2025-09-17 21:15:24 +0530

    test

---
+++ Terraform/Azure/infra/modules/eventgrid/main.tf
@@ -0,0 +1,26 @@
+resource "azurerm_eventgrid_event_subscription" "blob_created_trigger" {
+  name  = var.subscription_name
+  scope = var.storage_account_id
+
+  event_delivery_schema = "EventGridSchema"
+  included_event_types  = ["Microsoft.Storage.BlobCreated"]
+
+  destination {
+    endpoint_type = "AzureFunction"
+    resource_id   = var.function_app_id
+  }
+
+  advanced_filter {
+    key      = "data.url"
+    operator = "StringEndsWith"
+    values   = [".parquet"]
+  }
+
+  retry_policy {
+    max_delivery_attempts = 5
+    event_time_to_live    = 1440
+  }
+
+  labels = var.labels
+
+}
---
+++ Terraform/Azure/infra/modules/eventgrid/variables.tf
@@ -0,0 +1,4 @@
+variable "subscription_name" {}
+variable "storage_account_id" {}
+variable "function_app_id" {}
+variable "labels" {}
