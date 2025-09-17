ff54bf84f10013a46c7eb2f6cc09ad1eb457b50e
Mamud Hasan <mail2mamud@gmail.com>
2025-09-17 09:16:43 +0530

    changes

--- Terraform/Azure/infra/modules/function_app/main.tf
+++ Terraform/Azure/infra/modules/function_app/main.tf
@@ -19,38 +19,3 @@ resource "azurerm_application_insights" "insights" {
   workspace_id        = var.log_analytics_workspace_id
 }
 
-
-resource "azurerm_linux_function_app" "function_app" {
-  name                       = var.function_app_name
-  location                   = var.resource_group_location
-  resource_group_name        = var.resource_group_name
-  
-  service_plan_id            = azurerm_service_plan.plan.id
-  
-  storage_account_name       = var.storage_account_name
-  storage_account_access_key = var.storage_account_access_key
-
-
-
-  functions_extension_version = "~4"
-
-  site_config {
-    application_stack {
-      python_version = "3.10"
-    }
-  }
-
-  app_settings = {
-    FUNCTIONS_WORKER_RUNTIME        = "python"
-    AzureWebJobsStorage             = var.storage_connection_string
-    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.insights.instrumentation_key
-  }
-
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  tags = var.tags
-}
-
