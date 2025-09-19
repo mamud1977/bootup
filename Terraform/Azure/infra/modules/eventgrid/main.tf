cfaa88a5da4f76fac9e14672bd63edc8f9e78eda
Mamud Hasan <mail2mamud@gmail.com>
2025-09-19 13:36:03 +0530

    changes

--- Terraform/Azure/infra/modules/eventgrid/main.tf
+++ Terraform/Azure/infra/modules/eventgrid/main.tf
@@ -1,15 +1,16 @@
 resource "azurerm_eventgrid_event_subscription" "eventgrid_triggerd" {
   name  = var.subscription_name
   scope = var.storage_account_id
-  included_event_types = ["Microsoft.Storage.BlobCreated"]
+  included_event_types = [
+    "Microsoft.Storage.BlobCreated",
+    "Microsoft.Storage.BlobDeleted"
+    ]
+  
   event_delivery_schema = "EventGridSchema"
 
-  subject_filter {
-    subject_ends_with = ".txt"
-  }
 
   azure_function_endpoint {
-    function_id = "${var.function_app_id}/functions/${var.function_name-eventgrid-triggered}"
+    function_id = "${var.function_app_id}/functions/${var.function_name_eventgrid_triggered}"
   }
 
   labels = var.labels
--- Terraform/Azure/infra/modules/eventgrid/variables.tf
+++ Terraform/Azure/infra/modules/eventgrid/variables.tf
@@ -2,7 +2,7 @@ variable "subscription_name" {}
 variable "storage_account_id" {}
 variable "function_app_id" {}
 
-variable "function_name-eventgrid-triggered" {
+variable "function_name_eventgrid_triggered" {
   description = "Name of azure function"
   type        = string
   default     = "eventgrid-triggered"
