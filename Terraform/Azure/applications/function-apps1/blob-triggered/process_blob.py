2ffb1540e3c10da2a224a17b89c9f9e5715d98cf
Mamud Hasan <mail2mamud@gmail.com>
2025-09-19 10:08:40 +0530

    changes

--- Terraform/Azure/applications/function-apps1/blob-triggered/process_blob.py
+++
@@ -1,8 +0,0 @@
-import logging
-import pyarrow.parquet as pq
-import io
-import azure.functions as func
-
-def main(myblob: func.InputStream):
-    logging.info(f"Blob trigger fired for: {myblob.name}")
-    logging.info(f"Blob size: {myblob.length} bytes")
