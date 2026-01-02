c94087c63969e48c50a8213bca4335f4e894812a
Mamud Hasan <mail2mamud@gmail.com>
2026-01-02 23:25:35 +0530

    cleanup

--- FastApi/database/connection.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-DB_PATH = "database/sqlite.db"
-
-def get_connection():
-    conn = sqlite3.connect(DB_PATH)
-    conn.row_factory = sqlite3.Row  # Enables dict-like access
-    return conn
--- FastApi/db/schema.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, Field, field_validator
-from typing import Optional, List
-
-class loginProfile(BaseModel):
-    username: str
-    password: str
-
